67023b57a07ce7b63713a4b9b91934c813d10017
Bruno Melo <bruno.b.melo@live.com>
2021-03-15 22:12:28 -0300

    more performance

--- Program.cs
+++ Program.cs
@@ -14,12 +14,12 @@ namespace FastProcess
             var gen1 = GC.CollectionCount(1);
             var gen2 = GC.CollectionCount(2);
 
-            byte[] freelancer = Encoding.UTF8.GetBytes("FREE-LANCER");
-            byte[] consult = Encoding.UTF8.GetBytes("CONSULTOR");
-            byte[] pde = Encoding.UTF8.GetBytes("PDE");
+            Span<byte> freelancer = Encoding.UTF8.GetBytes("FREE-LANCER");
+            Span<byte> consult = Encoding.UTF8.GetBytes("CONSULTOR");
+            Span<byte> pde = Encoding.UTF8.GetBytes("PDE");
+            Span<byte> line, responsibility, ammountSale, totalSale;
             Span<decimal> totalSales = stackalloc decimal[3];
             Span<int> ammounts = stackalloc int[3];
-            Span<byte> line, responsibility, ammountSale, totalSale;
 
             int bytesOffSet = 0;
             int bytesConsumed = 0;
@@ -41,18 +41,18 @@ namespace FastProcess
                     var (rIndex, rLength) = GetPosition(line, 2);
                     responsibility = line.Slice(rIndex, rLength);
 
-                    var (aIndex, aLength) = GetPosition(line,4);
+                    var (aIndex, aLength) = GetPosition(line, 4);
                     ammountSale = line.Slice(aIndex, aLength);
 
-                    var (tIndex, tLength) = GetPosition(line,5);
+                    var (tIndex, tLength) = GetPosition(line, 5);
                     totalSale = line[tIndex..];
 
                     if (responsibility.SequenceEqual(freelancer))
-                        ExecuteCalculation(ammountSale, totalSale, ref ammounts[0], ref totalSales[0]);
-                    else if(responsibility.SequenceEqual(consult))
-                        ExecuteCalculation(ammountSale, totalSale, ref ammounts[1], ref totalSales[1]);
-                    else if(responsibility.SequenceEqual(pde))
-                        ExecuteCalculation(ammountSale, totalSale, ref ammounts[2], ref totalSales[2]);
+                        ExecuteCalculation(ammountSale, ref ammounts[0], totalSale, ref totalSales[0]);
+                    else if (responsibility.SequenceEqual(consult))
+                        ExecuteCalculation(ammountSale, ref ammounts[1], totalSale, ref totalSales[1]);
+                    else if (responsibility.SequenceEqual(pde))
+                        ExecuteCalculation(ammountSale, ref ammounts[2], totalSale, ref totalSales[2]);
 
                     bytesConsumed += position - bytesConsumed + 1;
                 }
@@ -111,10 +111,25 @@ namespace FastProcess
             return (start, end);
         }
 
-        internal static void ExecuteCalculation(Span<byte> ammounts, Span<byte> sales, ref int ammount, ref decimal totalSale)
+        internal static void ExecuteCalculation(Span<byte> ammounts, ref int ammout, Span<byte> sales, ref decimal totalSale)
         {
-            ammount += int.Parse(Encoding.UTF8.GetString(ammounts));
-            totalSale += decimal.Parse(Encoding.UTF8.GetString(sales));
+            Span<char> _char = stackalloc char[ammounts.Length];
+
+            for (int index = 0; index < ammounts.Length; index++)
+            {
+                _char[index] = (char)ammounts[index];
+            }
+
+            ammout += int.Parse(_char);
+
+            _char = stackalloc char[sales.Length];
+
+            for (int index = 0; index < sales.Length; index++)
+            {
+                _char[index] = (char)sales[index];
+            }
+
+            totalSale += decimal.Parse(_char);
         }
 
         internal static Span<byte> GetValueBetweenSemicolon(Span<byte> line, int positionInLine)
